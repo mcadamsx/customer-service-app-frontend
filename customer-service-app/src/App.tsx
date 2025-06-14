51856f9956f9540c95c4de834306f985e09bad17
PBentil <paapabentil122@gmail.com>
2025-06-14 23:23:09 +0000

    project set up and tailwind set up

---
+++ customer-service-app/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-center text-primary text-4xl">CUSTOMER SERVICE APP</h1>
+    </>
+  )
+}
+
+export default App
