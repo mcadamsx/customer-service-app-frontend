c88c4da9fe95f5f1fae8d477b6b99fcb4ec5c186
PBentil <paapabentil122@gmail.com>
2025-06-18 17:26:11 +0000

    fix: any types

--- src/components/common/Select.tsx
+++ src/components/common/Select.tsx
@@ -29,11 +29,9 @@ const SearchableSelect: React.FC<SearchableSelectProps> = ({
     const selectedLabel = options.find((opt) => opt.value === value)?.label || '';
 
     useEffect(() => {
-        // Keep searchTerm in sync with selected value label
         setSearchTerm(selectedLabel);
-    }, [value]);
+    }, [selectedLabel, value]);
 
-    // Close on outside click
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
             if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
